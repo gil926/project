98335dfb6d7986f3ef438935800467235fe2b29f
gil926 <82399605+gil926@users.noreply.github.com>
2022-05-22 21:44:40 +0300

    Create add.js

---
+++ views/add.js
@@ -0,0 +1,38 @@
+//add to cart//
+const add1 = document.getElementById("add");
+const add2 =document.getElementById("add1");
+const add3 = document.getElementById("add2");
+const add4 = document.getElementById("add3");
+const add5 = document.getElementById("add4");
+const add6 = document.getElementById("add5");
+let cart = 0;
+add1.onclick=function(){
+  cart +=1;
+  alert("an item has been added to the cart");
+  alert("items in cart"+cart);
+}
+add2.onclick=function(){
+    cart +=1;
+    alert("an item has been added to the cart");
+    alert("items in cart"+cart);
+  }
+add3.onclick=function(){
+    cart +=1;
+    alert("an item has been added to the cart");
+    alert("items in cart"+cart);
+  }
+  add4.onclick=function(){
+    cart +=1;
+    alert("an item has been added to the cart");
+    alert("items in cart"+cart);
+  }
+  add5.onclick=function(){
+    cart +=1;
+    alert("an item has been added to the cart");
+    alert("items in cart"+cart);
+  }
+  add6.onclick=function(){
+    cart +=1;
+    alert("an item has been added to the cart");
+    alert("items in cart"+cart);
+  }
